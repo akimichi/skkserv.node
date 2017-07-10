bad3fa53b5d4f8581a50dd4fd784fc2760eac061
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2017-07-11 01:31:46 +0900

    before refactoring service

--- models/entry.js
+++ models/entry.js
@@ -4,31 +4,19 @@
 const mongoose     = require('mongoose');
 const Schema       = mongoose.Schema;
 
-// const CandicateSchema   = new Schema({
-//   word: { type: String, required: false},
-//   createdOn: {type: Date, default: Date.now, required: true}
-// });
-
 const EntrySchema   = new Schema({
   yomi: {type: String, unique: true, required: true},
   candidates: Array,
-  // SubDocument: Koho
-  // candidates: [
-  //   CandicateSchema
-  // ]
   createdOn: {type: Date, default: Date.now, required: true}
 });
 
 EntrySchema.statics.henkan = function (yomi, callback) {
-  // this.findOne({ 'yomi': yomi }, 'yomi candidates', function (err, entry) {
   this.findOne({ 'yomi': yomi }, function (err, entry) {
     if (err) {
       callback(err);
     } else if (entry) {
-      console.log(`entry: ${entry}`);
       const candidates = entry.candidates.join("/");
-      const response = `1/${candidates}/\n`;
-      callback(null, response);
+      callback(null, `1/${candidates}/\n`);
     } else {
       callback(null, `4${yomi} `);
     }

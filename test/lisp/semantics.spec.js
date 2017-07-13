eee52e1149eb6529583d6e3ab2db3496102f171c
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2017-07-13 23:43:52 +0900

    implement boolean

--- test/lisp/semantics.spec.js
+++ test/lisp/semantics.spec.js
@@ -2,18 +2,60 @@
 
 const expect = require('expect.js'),
  exp = require('../../lib/lisp/exp.js'),
- ID = require('../../lib/lisp/monad.js').ID,
- semantics = require('../../lib/lisp/semantics.js');
+ env = require('../../lib/lisp/env.js'),
+ ID = require('../../lib/lisp/monad.js').ID;
+const evaluate = require('../../lib/lisp/semantics.js');
 
-describe('式', () => {
-  const env = semantics.env;
+describe('式の評価', () => {
 
   it('数値の評価のテスト', (next) => {
     expect(
-      semantics.evaluate(exp.num(2), env.empty)
+      evaluate(exp.num(2), env.empty)
     ).to.eql(
       ID.unit(2)
     );
     next();
+  });
+  it('ブール値の評価のテスト', (next) => {
+    expect(
+      evaluate(exp.bool(true), env.empty)
+    ).to.eql(
+      ID.unit(true)
+    );
+    next();
+  });
+  describe('ブール演算のテスト', () => {
+    it('andのテスト', (next) => {
+      expect(
+        evaluate(exp.and(
+            exp.bool(true),exp.bool(true)
+        ), env.empty)
+      ).to.eql(
+        ID.unit(true)
+      );
+      next();
+    });
+    it('orのテスト', (next) => {
+      expect(
+        evaluate(exp.or(
+            exp.bool(false),exp.bool(false)
+        ), env.empty)
+      ).to.eql(
+        ID.unit(false)
+      );
+      next();
+    });
+  });
+  describe('数値演算のテスト', () => {
+    it('addのテスト', (next) => {
+      expect(
+        evaluate(exp.add(
+            exp.num(0),exp.num(1)
+        ), env.empty)
+      ).to.eql(
+        ID.unit(1)
+      );
+      next();
+    });
   });
 });

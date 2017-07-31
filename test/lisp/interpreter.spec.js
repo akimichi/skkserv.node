2711b1af5479861bd29d002995f92e69594d9a99
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2017-07-31 17:05:48 +0900

    working on interpreter test

--- test/lisp/interpreter.spec.js
+++ test/lisp/interpreter.spec.js
@@ -7,7 +7,10 @@ const expect = require('expect.js'),
   List = require('kansuu.js').monad.list,
   Parser = require('../../lib/lisp/parser.js');
 
-const interpret = require('../../lib/lisp/interpreter.js');
+const Env = require('../../lib/lisp/interpret.js').env,
+  Exp = require('../../lib/lisp/interpret.js').exp,
+  Parser = require('../../lib/lisp/interpret.js').parser,
+  Evalator = require('../../lib/lisp/interpret.js').evaluator;
 
 describe('インタープリター', () => {
   it('数値の評価のテスト', (next) => {
@@ -23,3 +26,20 @@ describe('インタープリター', () => {
     next();
   });
 });
+
+// const interpret = require('../../lib/lisp/interpreter.js');
+
+// describe('インタープリター', () => {
+//   it('数値の評価のテスト', (next) => {
+//     const numericParser = Parser.numeric();
+//     Either.match(interpret(numericParser)("123")(env.empty), {
+//       left: (value) => {
+//         expect().fail();
+//       },
+//       right: (value) => {
+//         expect(value).to.eql(123)
+//       }
+//     });
+//     next();
+//   });
+// });

67a22673ec79de9714e086c5135ed813fff5cb77
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2018-02-08 23:13:16 +0900

    refactoring seed.js

--- bin/seed.js
+++ bin/seed.js
@@ -7,8 +7,9 @@
 // 
 
 const env = process.env.NODE_ENV,
- config = require('../lib/config.js')(env);
-const uri = config.db.mongo.uri;
+ config = require('../lib/config.js')(env),
+ uri = config.db.mongo.uri;
+
 console.log(`mongodb uri: ${uri}`)
 const loadDictionary = require('../lib/loadDictionary.js');
 
@@ -23,7 +24,10 @@ const DB = mongoose.connect(
   options
 );
 
+const MongoClient = require('mongodb').MongoClient;
+
 const Entry = require('../models/entry.js');
+
 
 const callback = (line) => {
   if(/^;;.+$/.test(line) == false) {
@@ -53,49 +57,51 @@ const callback = (line) => {
   }
 };
 
-// あらかじめデータを消去しておく
-Entry.remove({},(err) => {
-  if(err) {
-    throw new Error(err)
-  } else {
-    console.log('Entry removed');
-    loadDictionary.load('./resource/SKK-JISYO.L', (err, result) => {
-      if(err) {
-        db.close();
-        process.exit();
-      } else {
-        loadDictionary.load('./resource/SKK-JISYO.fullname', (err, result) => {
-          if(err) {
-            db.close();
-            process.exit();
-          } else {
-            db.close();
-            process.exit();
-          } 
-        });
-      }
-    });
-    // loadDictionary.load('./resource/SKK-JISYO.ML');
-    // // loadDictionary.load('./resource/SKK-JISYO.S', callback);
-    // loadDictionary.load('./resource/SKK-JISYO.M', callback);
-    // loadDictionary.load('./resource/SKK-JISYO.fullname');
-    // loadDictionary.load('./resource/SKK-JISYO.jinmei');
-    // loadDictionary.load('./resource/SKK-JISYO.propernoun', callback);
-    // loadDictionary.load('./resource/SKK-JISYO.geo');
-    // loadDictionary.load('./resource/SKK-JISYO.station');
-    // loadDictionary.load('./resource/SKK-JISYO.zipcode');
-    // loadDictionary.load('./resource/SKK-JISYO.mazegaki', callback);
-    // loadDictionary.load('./resource/SKK-JISYO.law');
-    // // loadDictionary.load('./resource/SKK-JISYO.lisp', callback);
-    // // loadDictionary.load('./resource/SKK-JISYO.JIS2', callback);
-    // // loadDictionary.load('./resource/SKK-JISYO.JIS2004', callback);
-    // loadDictionary.load('./resource/SKK-JISYO.JIS3_4', callback);
-  }
-})
+MongoClient.connect(uri, (err, db) => {
+  console.log(`connecting ${uri}`)
+  if (err) return console.error(err);
+  const collection = db.collection('entries');
 
-
-
-
+  // あらかじめデータを消去しておく
+  Entry.remove({},(err) => {
+    if(err) {
+      throw new Error(err)
+    } else {
+      console.log('Entry removed');
+      loadDictionary.load('./resource/SKK-JISYO.L', (err, result) => {
+        if(err) {
+          db.close();
+          process.exit();
+        } else {
+          loadDictionary.load('./resource/SKK-JISYO.fullname', (err, result) => {
+            if(err) {
+              db.close();
+              process.exit();
+            } else {
+              db.close();
+              process.exit();
+            } 
+          });
+        }
+      });
+      // loadDictionary.load('./resource/SKK-JISYO.ML');
+      // // loadDictionary.load('./resource/SKK-JISYO.S', callback);
+      // loadDictionary.load('./resource/SKK-JISYO.M', callback);
+      // loadDictionary.load('./resource/SKK-JISYO.fullname');
+      // loadDictionary.load('./resource/SKK-JISYO.jinmei');
+      // loadDictionary.load('./resource/SKK-JISYO.propernoun', callback);
+      // loadDictionary.load('./resource/SKK-JISYO.geo');
+      // loadDictionary.load('./resource/SKK-JISYO.station');
+      // loadDictionary.load('./resource/SKK-JISYO.zipcode');
+      // loadDictionary.load('./resource/SKK-JISYO.mazegaki', callback);
+      // loadDictionary.load('./resource/SKK-JISYO.law');
+      // // loadDictionary.load('./resource/SKK-JISYO.lisp', callback);
+      // // loadDictionary.load('./resource/SKK-JISYO.JIS2', callback);
+      // // loadDictionary.load('./resource/SKK-JISYO.JIS2004', callback);
+      // loadDictionary.load('./resource/SKK-JISYO.JIS3_4', callback);
+    }
+  })
+});
 
 process.on('SIGINT', () =>  {
   mongoose.connection.close(() => {
